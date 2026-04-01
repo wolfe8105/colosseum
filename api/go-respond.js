7434ea5dfd3bdf4b93829153f044d8e89deffde0
wolfe8105 <wolfe8105@gmail.com>
2026-03-31 20:58:27 -0400

    Update go-respond.js

--- api/go-respond.js
+++ api/go-respond.js
@@ -73,7 +73,7 @@ Respond ONLY with valid JSON, no markdown, no backticks:
 
 export default async function handler(req, res) {
   // CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://themoderator.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
